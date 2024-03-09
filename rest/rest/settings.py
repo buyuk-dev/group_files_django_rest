912a79c089d414a0347d52083545ca24bbd5c39a
Michal Michalski <michal@buyuk-dev.com>
2024-03-09 14:43:06 +0100

    Move api app into project folder; Create accounts app and setup custom user model;

--- rest/rest/settings.py
+++ rest/rest/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +84,10 @@ DATABASES = {
 }
 
 
+# Configure user model for auth.
+AUTH_USER_MODEL = 'accounts.User'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -98,7 +105,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
