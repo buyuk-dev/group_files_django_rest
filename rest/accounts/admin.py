1ed94bfce720fac3ee8e505be712d012cfd61564
Michal Michalski <michal@buyuk-dev.com>
2024-03-09 15:27:35 +0100

    register custom user in django admin panel;

--- rest/accounts/admin.py
+++ rest/accounts/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'is_staff', 'is_active',]  # Customize as needed
+    list_filter = ['email', 'is_staff', 'is_active',]  # Customize as needed
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ()}),  # Customize with your fields
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(User, CustomUserAdmin)
