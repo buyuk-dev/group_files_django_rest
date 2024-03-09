662cdd377f3282422f4f837ce1c7c8e32dddf468
Michal Michalski <michal@buyuk-dev.com>
2024-03-09 16:15:46 +0100

    Configure drf-yasg for api docs generation; Add users api endpoints;

--- rest/accounts/views.py
+++ rest/accounts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import status, viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    A viewset for viewing and editing user instances.
+    """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [AllowAny()]
+        return super().get_permissions()
+
+
+class UserListView(APIView):
+    """
+    View to list all users in the system.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
+    """
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        """
+        Return a list of all users.
+        """
+        usernames = [user.id for user in User.objects.all()]
+        return Response(usernames)
--- rest/rest/urls.py
+++ rest/rest/urls.py
@@ -15,8 +15,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from rest_framework import permissions
+from rest_framework.routers import DefaultRouter
+
+from accounts.views import UserViewSet, UserListView
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="File Grouping API",
+        default_version='v1',
+        description="This api is used to group files represented by paths separated using characters defined in delimiters characters set into folders.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="michal@buyuk-dev.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include(router.urls)),
+    path('api/users/list/', UserListView.as_view(), name='user-list'),
 ]
